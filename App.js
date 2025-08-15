8eea375d1288aada3a43b47b9fa994f19bd759ae
rrochin <rrochin_22@alu.uabcs.mx>
2025-08-15 13:36:30 -0700

    Listo, aplicacion sencilla

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, Alert } from 'react-native';
 
 export default function App() {
 
-  var texto = "hola";
+  const showAlert = () => {
+    Alert.alert('¡No tan fuerte :(!');
+  };
 
   return (
     <View style={styles.container}>
-      <Text style={styles.texto}>{texto}</Text>
-      <Text>Hola mundo</Text>
+      <Text style={styles.title}>Mi primera app con React Native</Text>
+
+      <TouchableOpacity style={styles.button} onPress={showAlert}>
+        <Text style={styles.buttonText}>Presióname</Text>
+      </TouchableOpacity>
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -20,10 +26,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20
   },
-
-texto: {
-  color: '#FF0002',
-  fontSize: 20,
-}
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  texto: {
+    marginTop: 20,
+    fontSize: 18,
+  },
+  button: {
+    backgroundColor: '#F8BBD0', 
+    paddingVertical: 12,
+    paddingHorizontal: 25,
+    borderRadius: 25, 
+    marginBottom: 20,
+  },
+  buttonText: {
+    color: '#fff', 
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
